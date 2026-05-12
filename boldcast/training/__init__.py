3e39b491995f5b9ee9322ec9d9058f6d0b9a103e
Yibei Chen <yibeichan@gmail.com>
2026-05-12 17:48:22 -0400

    chore(training): re-export public API from boldcast.training

---
+++ boldcast/training/__init__.py
@@ -0,0 +1,22 @@
+"""Training subpackage: loss, optimizer/scheduler factories, Trainer, utils.
+
+Re-exports the public API surface so Day-4 / Day-5 scripts can write
+``from boldcast.training import Trainer, build_optimizer, ...`` rather
+than importing each submodule by name.
+"""
+
+from boldcast.training.loss import build_forecast_targets, forecasting_loss
+from boldcast.training.optim import build_optimizer, build_scheduler
+from boldcast.training.trainer import Trainer
+from boldcast.training.utils import JsonlLogger, save_checkpoint, seed_everything
+
+__all__ = [
+    "JsonlLogger",
+    "Trainer",
+    "build_forecast_targets",
+    "build_optimizer",
+    "build_scheduler",
+    "forecasting_loss",
+    "save_checkpoint",
+    "seed_everything",
+]
